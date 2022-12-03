ee839f25506a731544b79730788256c0eee2d17c
Ezra Magbanua <magbanuaezra@gmail.com>
2022-12-03 11:37:42 +0800

    misc

---
+++ components/Layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface PropsInterface {
+    children: JSX.Element | JSX.Element[];
+}
+
+function Layout(props: PropsInterface) {
+    const { children } = props;
+
+    return (
+        <div className="px-5 sm:px-20 md:px-32 xl:px-72 py-10">
+            <h1 className="text-5xl my-5">PoStQL</h1>
+            <main>{children}</main>
+        </div>
+    );
+}
+
+export default Layout;
