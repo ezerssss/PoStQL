ff66a9f9670a2892ba260311425847443d59c3f6
trander123 <trander123@yahoo.com>
2022-12-03 11:50:56 +0800

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <h1 className="text-xl">Hello world</h1>
+            <h1 className="text-xl">Hello world</h1>z
         </div>
     );
 }
