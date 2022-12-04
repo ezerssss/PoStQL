de3f8347e804303d7e6cd12c806cc3021628d602
Ezra Magbanua <magbanuaezra@gmail.com>
2022-12-04 19:18:50 +0800

    [Feature][Ezra] get post from db

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,24 +1,47 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { StatusCodes } from 'http-status-codes';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { POSTS_PER_PAGE } from '../../constants/page';
 import db from '../../db/db';
+import { Post } from '../../interfaces/post';
 
-type Data = {
-    name: string;
-};
+async function get(
+    req: NextApiRequest,
+    res: NextApiResponse<{ posts: Post[] }>,
+) {
+    try {
+        const { page, type, order = 'ASC' } = req.query;
+        let offset = 0;
+        if (page && typeof page === 'string') {
+            offset = (parseInt(page) - 1) * POSTS_PER_PAGE;
+        }
+
+        let types = [''];
+        if (type && typeof type == 'string') {
+            types = type.split(',');
+        }
+
+        const response = (await db.query(
+            `SELECT content, date, name AS type FROM posts JOIN post_type USING (post_type_id) WHERE name IN (?) ORDER BY date ${order} LIMIT ?, ${POSTS_PER_PAGE}`,
+            [types, offset],
+        )) as Post[];
+        await db.end();
+
+        const posts = response.map(({ content, type, date }) => ({
+            content,
+            type,
+            date,
+        }));
+
+        res.status(StatusCodes.OK).json({ posts });
+    } catch (error) {
+        console.error(error);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ posts: [] });
+    }
+}
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<Data>,
+    res: NextApiResponse<{ posts: Post[] }>,
 ) {
-    try {
-        const results = await db.query(
-            'SELECT * FROM posts JOIN post_type USING (post_type_id)',
-        );
-        await db.end();
-        console.log(results);
-        res.status(200).json({ name: 'John Doe' });
-    } catch (error) {
-        console.error(error);
-        res.status(400).json({ name: 'Ezra Magbanua' });
-    }
+    if (req.method === 'GET') await get(req, res);
 }
