187e52db8dc0c19387fdea0b005dfd987e9af8e5
Ezra Magbanua <magbanuaezra@gmail.com>
2022-12-05 21:22:26 +0800

    [Feature][Ezra] pagination

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -8,16 +8,14 @@ interface PropsInterface {
 
 export function PostCard({ post }: PropsInterface) {
     return (
-        <div className=" bg-gray-200 rounded-xl px-8 py-4">
-            <div className=" text-center">
-                <h2 className=" font-bold">{post.type}</h2>
-                <p className=" font-thin text-gray-500 text-xs">
-                    {dateFormatter(post.date)}
-                </p>
-            </div>
-            <div className=" mt-5">
+        <div className="bg-gray-200 rounded-xl p-4 border border-black">
+            <div className="mb-5">
                 <p>{post.content}</p>
             </div>
+            <h2 className="font-bold">{post.type}</h2>
+            <p className="font-thin text-gray-500 text-xs">
+                {dateFormatter(post.date)}
+            </p>
         </div>
     );
 }
--- components/PostList.tsx
+++ components/PostList.tsx
@@ -7,21 +7,49 @@ import { PostCard } from './PostCard';
 
 export function PostList() {
     const [posts, setPosts] = useState<Post[]>([]);
+    const [pagesCount, setPagesCount] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
 
     useEffect(() => {
         async function getPosts() {
-            const res = await axios.get(BASE_API_ENDPOINT);
+            const res = await axios.get(BASE_API_ENDPOINT, {
+                params: { page: currentPage },
+            });
             const data = res.data as GetAPI;
             setPosts(data.posts);
+            setPagesCount(data.pages);
         }
+
         getPosts();
-    }, []);
+    }, [currentPage]);
+
+    const isPaginationButtonsDisabled = !pagesCount;
+    const isPrevDisabled = currentPage == 1 || isPaginationButtonsDisabled;
+    const isNextDisabled =
+        currentPage >= pagesCount || isPaginationButtonsDisabled;
 
     return (
         <div className=" space-y-5 mt-5">
             {posts.map((post, index) => (
                 <PostCard key={`${post.date}${index}`} post={post} />
             ))}
+
+            <div className="flex gap-4">
+                <button
+                    className="disabled:text-gray-400"
+                    disabled={isPrevDisabled}
+                    onClick={() => setCurrentPage((prev) => prev - 1)}
+                >
+                    Prev
+                </button>
+                <button
+                    className="disabled:text-gray-400"
+                    disabled={isNextDisabled}
+                    onClick={() => setCurrentPage((prev) => prev + 1)}
+                >
+                    Next
+                </button>
+            </div>
         </div>
     );
 }
